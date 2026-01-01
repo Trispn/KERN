43c2ce6a904bb9c43daa38291348e35026c09c1b
Trispn <Trispnx@gmail.com>
2026-01-01 20:58:04 +0300

    build: Add generated Rust compilation artifacts including incremental compilation data, object files, and dependency metadata.

---
+++ target/debug/deps/futures_sink-4c230b442e9b2912.d
@@ -0,0 +1,7 @@
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\deps\futures_sink-4c230b442e9b2912.d: C:\Users\LENOVO\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\futures-sink-0.3.31\src\lib.rs
+
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\deps\libfutures_sink-4c230b442e9b2912.rlib: C:\Users\LENOVO\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\futures-sink-0.3.31\src\lib.rs
+
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\deps\libfutures_sink-4c230b442e9b2912.rmeta: C:\Users\LENOVO\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\futures-sink-0.3.31\src\lib.rs
+
+C:\Users\LENOVO\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\futures-sink-0.3.31\src\lib.rs:
---
+++ target/debug/deps/kern_compiler-057f94c8b24dc978.d
@@ -0,0 +1,20 @@
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\deps\kern_compiler-057f94c8b24dc978.d: src\lib.rs src\lexer\token_kind.rs src\lexer\token.rs src\lexer\keywords.rs src\lexer\lexer.rs src\parser\ast_nodes.rs src\parser\parser_state.rs src\parser\precedence.rs src\parser\parser_error.rs src\parser\parser.rs src\shared\source_location.rs src\shared\diagnostics.rs src\shared\string_interner.rs src\shared\config.rs
+
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\deps\libkern_compiler-057f94c8b24dc978.rlib: src\lib.rs src\lexer\token_kind.rs src\lexer\token.rs src\lexer\keywords.rs src\lexer\lexer.rs src\parser\ast_nodes.rs src\parser\parser_state.rs src\parser\precedence.rs src\parser\parser_error.rs src\parser\parser.rs src\shared\source_location.rs src\shared\diagnostics.rs src\shared\string_interner.rs src\shared\config.rs
+
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\deps\libkern_compiler-057f94c8b24dc978.rmeta: src\lib.rs src\lexer\token_kind.rs src\lexer\token.rs src\lexer\keywords.rs src\lexer\lexer.rs src\parser\ast_nodes.rs src\parser\parser_state.rs src\parser\precedence.rs src\parser\parser_error.rs src\parser\parser.rs src\shared\source_location.rs src\shared\diagnostics.rs src\shared\string_interner.rs src\shared\config.rs
+
+src\lib.rs:
+src\lexer\token_kind.rs:
+src\lexer\token.rs:
+src\lexer\keywords.rs:
+src\lexer\lexer.rs:
+src\parser\ast_nodes.rs:
+src\parser\parser_state.rs:
+src\parser\precedence.rs:
+src\parser\parser_error.rs:
+src\parser\parser.rs:
+src\shared\source_location.rs:
+src\shared\diagnostics.rs:
+src\shared\string_interner.rs:
+src\shared\config.rs:
