ab81948e0f5a096ad29c4453eaae3c1e857b017e
Trispn <Trispnx@gmail.com>
2026-01-02 12:06:48 +0300

    build: Generate compilation and test artifacts for KERN modules.

---
+++ target/debug/deps/grammar_verification-9aeeadff176244a7.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\deps\grammar_verification-9aeeadff176244a7.d: kern-parser\tests\grammar_verification.rs
+
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\deps\grammar_verification-9aeeadff176244a7.exe: kern-parser\tests\grammar_verification.rs
+
+kern-parser\tests\grammar_verification.rs:
---
+++ target/debug/examples/comprehensive_test.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\examples\comprehensive_test.d: kern-parser\examples\comprehensive_test.rs
+
+C:\Users\LENOVO\OneDrive\Desktop\KERN\target\debug\examples\comprehensive_test.exe: kern-parser\examples\comprehensive_test.rs
+
+kern-parser\examples\comprehensive_test.rs:
